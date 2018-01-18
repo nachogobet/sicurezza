3033ca69aee9366e58e6c109ab6ad93a9d59e923
Ignacio Gobet <ignaciogobet@gmail.com>
2018-01-18 17:24:01 -0300

    dsfsdz

--- src/com/ig/sicurezza/config/DatabaseConfig.java
+++ src/com/ig/sicurezza/config/DatabaseConfig.java
@@ -3,17 +3,17 @@ package com.ig.sicurezza.config;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseConfig {
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.url("postgres://gfxpoqbprezear:ab56e5ea6667591f5ea764bdd02a12bc37587cf1e843426cec5cc27e049ae5aa@ec2-54-243-193-227.compute-1.amazonaws.com:5432/d6nlqjcjbj2lqr");
-		dataSourceBuilder.username("gfxpoqbprezear");
-		dataSourceBuilder.password("ab56e5ea6667591f5ea764bdd02a12bc37587cf1e843426cec5cc27e049ae5aa");
-		return dataSourceBuilder.build();   
-	}
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
